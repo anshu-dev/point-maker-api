dff923ff234305027105e91c2210147618e068c1
Anshu <anshu902kumar@gmail.com>
2021-05-11 20:42:52 +0530

    Added user_params in sessionscontroller

--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -2,7 +2,7 @@ class  Api::Users::SessionsController < Devise::SessionsController
   before_action :set_user, on: :create
 
   def create
-    if @user && @user.valid_password?(sign_in_params[:password])
+    if @user && @user.valid_password?(user_params[:password])
       @current_user = @user
     else
       render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
@@ -14,7 +14,14 @@ class  Api::Users::SessionsController < Devise::SessionsController
 
   private
 
+  def user_params
+    params.permit(
+      :email,
+      :password
+    )
+  end
+
   def set_user
-    @user = User.find_by_email(sign_in_params[:email])
+    @user = User.find_by_email(user_params[:email])
   end
 end
