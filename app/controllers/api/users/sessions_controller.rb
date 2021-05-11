5f545b1c443a90ce78f6dd2db12bb2fde638ec28
Anshu <anshu902kumar@gmail.com>
2021-05-11 20:53:18 +0530

    Updated sessions controller

--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -1,27 +1,2 @@
 class  Api::Users::SessionsController < Devise::SessionsController
-  before_action :set_user, on: :create
-
-  def create
-    if @user && @user.valid_password?(user_params[:password])
-      @current_user = @user
-    else
-      render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-  end
-
-  private
-
-  def user_params
-    params.permit(
-      :email,
-      :password
-    )
-  end
-
-  def set_user
-    @user = User.find_by_email(user_params[:email])
-  end
 end
