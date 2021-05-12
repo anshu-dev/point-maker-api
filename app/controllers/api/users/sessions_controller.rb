046dd4134930d76e68abcf3bfe61125dfcc985cb
Anshu <anshu902kumar@gmail.com>
2021-05-12 12:27:05 +0530

    Added session create action

--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -1,2 +1,12 @@
 class  Api::Users::SessionsController < Devise::SessionsController
+  skip_before_action :doorkeeper_authorize!
+
+  def create
+    @user = User.find_by_email(params[:username])
+    if @user && @user.valid_password?(params[:password])
+      @user
+    else
+      render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
+    end
+  end
 end
