ea8ccff08af818c61019be64f1d4472d5847a934
Anshu <anshu902kumar@gmail.com>
2021-05-12 12:49:23 +0530

    Used find_for_database_authentication

--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -2,9 +2,9 @@ class  Api::Users::SessionsController < Devise::SessionsController
   skip_before_action :doorkeeper_authorize!
 
   def create
-    @user = User.find_by_email(params[:username])
+    @user = User.find_for_database_authentication(email: params[:username])
     if @user && @user.valid_password?(params[:password])
-      @user
+      render json: @user
     else
       render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
     end
