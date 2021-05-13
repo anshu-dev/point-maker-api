c5f16527a96cf6d7750159f090e9d2ec22627469
Anshu <anshu902kumar@gmail.com>
2021-05-13 19:24:33 +0530

    Added smtp configuration in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com',
+    :authentication     => :plain,
+    :user_name          => ENV['SMTP_USERNAME'],
+    :password           => ENV['SMTP_PASSWORD']
+  }
+  config.action_mailer.default_url_options = { host: ENV['FRONTEND_HOSTNAME'] }
 end
